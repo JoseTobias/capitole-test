50aa41c5bb9367d741949aa0e7ec4f33b98a6380
JoseTobias <tobiassouza0@gmail.com>
2025-11-24 16:28:19 -0300

    fix: fix error handling

--- app/handlers/getcatalogbycode/handler.go
+++ app/handlers/getcatalogbycode/handler.go
@@ -1,7 +1,9 @@
 package getcatalogbycode
 
 import (
+	"errors"
 	"github.com/mytheresa/go-hiring-challenge/app/api"
+	"github.com/mytheresa/go-hiring-challenge/app/usecase/catalogbycode"
 	"net/http"
 )
 
@@ -19,6 +21,10 @@ func (h *Handler) HandleGet(w http.ResponseWriter, r *http.Request) {
 	code := r.PathValue("code")
 	res, err := h.getter.GetByCode(code)
 	if err != nil {
+		if errors.Is(err, catalogbycode.ErrProductNotFound) {
+			api.ErrorResponse(w, http.StatusNotFound, err.Error())
+		}
+
 		api.ErrorResponse(w, http.StatusInternalServerError, err.Error())
 		return
 	}
